8f50ad79fc6db0544c49035284c34d5c4f5835bf
Robin <robin.pahlman@gmail.com>
2023-02-08 10:19:32 -0500

    addd end screen

---
+++ src/components/EndScreen.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>Good Job</h2>
+  <div>
+    <p>{{ quizResults }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  props: ["results"],
+  setup(props) {
+    const quizResults = ref(props.results);
+    return { quizResults };
+  },
+});
+</script>
+
+<style scoped></style>
--- src/components/RapidFire.vue
+++ src/components/RapidFire.vue
@@ -1,17 +1,19 @@
 <template>
   <StartScreen v-if="!quizStarted" @quizStarted="startHandler" />
   <component
-    v-if="quizStarted"
+    v-if="quizStarted && !quizCompleted"
     :quizData="quizData[step]"
     :key="step"
     :is="quizData[step].type"
     @submit="submitHandler"
   />
+  <EndScreen v-if="quizCompleted" :results="resultsStore" />
 </template>
 
 <script>
 import { reactive, ref } from "vue";
 import StartScreen from "./StartScreen.vue";
+import EndScreen from "./EndScreen.vue";
 import StandardQuestion from "./StandardQuestion.vue";
 export default {
   props: ["quizData"],
@@ -53,6 +55,7 @@ export default {
   components: {
     StartScreen,
     StandardQuestion,
+    EndScreen,
   },
 };
 </script>
